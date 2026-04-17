d534860af282ce8dc0039579351239d0b6cd08dc
Bryan Pinheiro <bryan.pinheiro77@hotmail.com>
2026-04-16 22:25:37 -0300

    chore: initial spring boot setup

---
+++ src/test/java/br/com/bookflow/BookflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bookflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
